cb2fe3df35a59fd8e5f11f0e333eeb84ad5531b8
amberbrown4 <zasalarian2000@gmail.com>
2020-12-24 22:41:03 +0330

    Set font and resolution

--- main.lua
+++ main.lua
@@ -1,11 +1,20 @@
 
+-- https://github.com/Ulydev/push
+push = require 'push'
+
 WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 480
-
+VIRTUAL_WIDTH = 332
+VIRTUAL_HEIGHT = 143
 
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+	retroFont = love.graphics.newFont('font.ttf', 16)
+	love.graphics.setFont(retroFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -14,10 +23,19 @@ end
 
 
 function love.draw()
+
+	push:apply('start')
     love.graphics.printf(
         'Hi Pong!',         
         0,                      
-        WINDOW_HEIGHT / 2 - 6,  
-        WINDOW_WIDTH,
-        'center')             
+        VIRTUAL_HEIGHT / 2 - 6,  
+        VIRTUAL_WIDTH,
+        'center')    
+	push:apply('end')
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
 end
