47e0c31b5a12e4e7c69c264654720fdbc5bc838e
amberbrown4 <zasalarian2000@gmail.com>
2020-12-24 21:51:58 +0330

    Managed height and width of the window

--- main.lua
+++ main.lua
@@ -1,3 +1,23 @@
+
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 480
+
+
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+
 function love.draw()
-    love.graphics.print("Hi", 400, 300)
+    love.graphics.printf(
+        'Hi Pong!',         
+        0,                      
+        WINDOW_HEIGHT / 2 - 6,  
+        WINDOW_WIDTH,
+        'center')             
 end
