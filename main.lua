f7f74d1fdbb30ef21ab8dcad71c8d1097ff0b353
amberbrown4 <zasalarian2000@gmail.com>
2020-12-26 20:24:53 +0330

    Added paddles

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 480
 VIRTUAL_WIDTH = 332
 VIRTUAL_HEIGHT = 143
+PADDLE_SPEED = 300
 
 
 function love.load()
@@ -19,18 +20,44 @@ function love.load()
         resizable = false,
         vsync = true
     })
+	
+	paddle1_y = 30
+	paddle2_y = VIRTUAL_HEIGHT - 50
 end
 
+function love.update(dt)
+	-- player1 movement
+	if love.keyboard.isDown('o') then -- moving up
+		paddle2_y = paddle2_y - PADDLE_SPEED*dt
+	elseif love.keyboard.isDown('l') then -- moving down
+		paddle2_y = paddle2_y + PADDLE_SPEED*dt
+	end
+	
+	-- player2 movement
+	if love.keyboard.isDown('w') then -- moving up
+		paddle1_y = paddle1_y - PADDLE_SPEED*dt
+	elseif love.keyboard.isDown('s') then -- moving down
+		paddle1_y = paddle1_y + PADDLE_SPEED*dt
+	end
+end
 
 function love.draw()
 
 	push:apply('start')
+	
+	-- love.graphics.clear(40, 45, 52, 255)
+
     love.graphics.printf(
         'Hi Pong!',         
         0,                      
         VIRTUAL_HEIGHT / 2 - 6,  
         VIRTUAL_WIDTH,
         'center')    
+	-- render first paddle
+	love.graphics.rectangle('fill', 10, paddle1_y, 5, 20)
+	-- render second paddle
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, paddle2_y, 5, 20)
+
 	push:apply('end')
 end
 
